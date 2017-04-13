1014c374901a5576fd6ec08e2bc726fcf6fdacce
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-04-13 20:01:54 +0530

    login and signup flow completed

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,7 +2,7 @@
 <div class="container">
 	<h1>Login to Continue</h1>
 	<div class="container">
-		<form class="col s12">
+		<form class="col s12" id="loginForm">
 			<div class="row">
 				<div class="input-field col s12">
 					<input placeholder="Username" v-model="username" type="text" class="validate">
@@ -13,9 +13,38 @@
 					<input placeholder="Password" v-model="password" type="password" class="validate">
 				</div>
 			</div>
+			<div class="row" style="text-align: left; margin-left: 1em;">
+				<a href="#" v-on:click="showSignup">Signup?</a>
+			</div>
 			<div class="row">
 				<div align="center">
-					<button type="submit" v-on:click="login" class="btn btn-waves">Submit</button>
+					<button type="submit" v-on:click="login" class="btn btn-waves">Login</button>
+				</div>
+			</div>
+		</form>
+		<form class="col s12" id="signupForm">
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Username" v-model="signupUsername" type="text" class="validate">
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Password" v-model="signupPassword" type="password" class="validate">
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Password" v-model="confirmSignupPassword" type="password" class="validate">
+				</div>
+			</div>
+			<span style="color: red;" v-if="confirmPassword">Passwords should match</span>
+			<div class="row" style="text-align: left; margin-left: 1em;">
+				<a href="#" v-on:click="showLogin">Login?</a>
+			</div>
+			<div class="row">
+				<div align="center">
+					<button type="submit" v-on:click="signup" class="btn btn-waves">Signup</button>
 				</div>
 			</div>
 		</form>
@@ -31,8 +60,11 @@ export default {
 	data () {
 		return {
 			username: '',
-			password: ''
-		}
+			password: '',
+			signupUsername: '',
+			signupPassword: '',
+			confirmSignupPassword: ''
+		};
 	},
 	methods: {
 		login(event) {
@@ -40,26 +72,67 @@ export default {
 			let user = {};
 			user.username = this.username,
 			user.password = this.password;
-			// console.log(user);
 			axios.post('/auth/login',user).then((response) => {
-				const data = response.body;
-				console.log(router);
-				if(!data.success){
-					console.log(this);
+				const data = response.data;
+				if(data.success){
+					console.log('Granted the login permission');
+					router.push('/home');
+				}
+				else{
+					console.log(`Unable to login because ${data.message}`);
 				}
 			})
 			.catch((err) => {
-				router.push('/home');
+				console.log('Some error occured while logging in');
+				console.log(err);
 			});
 			return false;
+		},
+		signup(event) {
+			event.preventDefault();
+			let user = {};
+			user.username = this.signupUsername;
+			user.password = this.signupPassword;
+			user.confirm = this.confirmSignupPassword;
+			if(user.confirm === user.password){
+				delete user.confirm; 
+				axios.post('/auth/signup',user).then((response) => {
+					console.log(response.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+			}
+			else{
+				console.log('Passwords don\'t match');
+			}
+		},
+		showSignup(event) {
+			event.preventDefault();
+			$('#loginForm').slideUp();
+			$('#signupForm').slideDown();
+			return false;
+		},
+		showLogin(event) {
+			event.preventDefault();
+			$('#loginForm').slideDown();
+			$('#signupForm').slideUp();
+			return false;
+		}
+	},
+	computed: {
+		confirmPassword() {
+			return this.signupPassword !== this.confirmSignupPassword;
 		}
 	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 label{
 	text-align: center !important;
+}
+form#signupForm{
+	display: none;
 }
 </style>
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -23,19 +23,25 @@ function signup(req,res){
 	* The user data is free from any thing that may cause some issue later on
 	*/
 	const users = db.get().collection('users');
-	users.findOne({}).then((person) => {
+	users.findOne({username: user.username}).then((person) => {
 		if(person){
 			return res.json({
 				success: false,
 				message: 'The username already exists in the database'
 			});
 		}
-		users.insertOne(user).then(() => {
+		bcrypt.hash(user.password, saltRounds).then((hash) => {
+			user.password = hash;
+			users.insertOne(user).then(() => {
 			console.log('User inserted successfully');
 			res.json({
 				success: true,
 				message: 'User inserted successfully'
 			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 		})
 		.catch((err) => {
 			console.log(err);
@@ -47,6 +53,26 @@ function signup(req,res){
 }
 
 
+/**
+ * @param {*} req - The standard express request object
+ * @param {*} res - The standard express response object
+ * In the below function, we get the username and password
+ * from the user and then check in the following manner
+ * 1) Check if the user exists in the database
+ * 2) If the user doesn't exist return response
+ * @return {json} res - {
+ * 		success: false,
+ * 		message: 'Username doesn't exist
+ * }
+ * 3) If the user exists then we go and check the password 
+ * 4) using bcrypt.compare function we check for the password
+ * 5) If the password matches then we allow the user
+ * to login by 
+ * @return {json} res - {
+ * 		success : true,
+ * 		message: 'Logged in succesfully'
+ * }
+ */
 function login(req,res){
 	
 	let user = req.body;
@@ -69,8 +95,8 @@ function login(req,res){
 			* Whenever any interaction happens in future over any of the secured routes, make
 			* sure to make an interceptor for all the requests so that the token is handled appropriately
 			*/
-			bcrypt.compare(user.password,person.password).then((res) => {
-				if(res){
+			bcrypt.compare(user.password,person.password).then((response) => {
+				if(response){
 					/**
 					* The user entered the correct password
 					*/
@@ -95,7 +121,11 @@ function login(req,res){
 			})
 		}
 		else{
-			return res.json({
+			/**
+			 * No person is found, this means that the username doesn't exist in the database
+			 * Now return a success false along with appropriate message
+			 */
+			res.json({
 				success: false,
 				message: 'The user doesn\'t exist in the database'
 			});
